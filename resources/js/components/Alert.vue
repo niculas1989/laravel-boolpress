add07fa418a02894da101e6bdf41f7f86ffb4fa3
niculas1989 <nicolas.maranzano@libero.it>
2022-04-06 15:09:55 +0100

    added more info on Alert

--- resources/js/components/Alert.vue
+++ resources/js/components/Alert.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="alert alert-danger" role="alert">Semplicemente un alert.</div>
+  <div :class="`alert alert-${type} || 'info'`" role="alert">{{ message }}</div>
 </template>
 
 <script>
 export default {
+  props: ["message", "type"],
   name: "Alert",
 };
 </script>
